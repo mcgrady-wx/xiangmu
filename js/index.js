6b2c445be660fecd8835cd6985abf5d003dcb42e
xiaoming <380198622@qq.com>
2019-09-09 17:49:16 +0800

    主体

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
-require(["config"], function(){
-	require(["jquery", "template", "load"], function($, template){
-		$.getJSON("/mock/list.json", function(data){
-			data = {list : data.res_body.data};
-			let html = template("list_template", data);
-			$(".list").html(html);
-		});
-	});
-});
+$(function(){
+	//轮播
+	var ads=$(".lun>li"),//获取所有药轮播得图片元素
+		len=ads.length,//获取图片总个数
+		startindex=0,//当前图片的下标
+		nextindex=1,//下一个要显示图片的下标
+		yuans=$(".yuan>i");//获得所有的小圆点
+	
+	
+	//轮播动画函数
+	var move=function(){
+		ads.forEach(function(){
+			ads[startindex].css("display","none");
+			ads[nextindex].css("display","block");
+		})
+		
+	};
+		
+	//启动计时器实现自动轮播
+	var timer=setInterval(move(),1000);
+}) 
