ad03534736963deb95a2c220aaa7702da6f64b7d
xiaoming <380198622@qq.com>
2019-09-10 17:19:21 +0800

    主体html完成

--- js/index.js
+++ js/index.js
@@ -7,55 +7,58 @@ $(function(){
 		yuans=$(".yuan>i"),//获得所有的小圆点
 		timer=null;
 		
-	//轮播动画函数
+	//动画函数
 	function move(){
-		//图片轮播
+		//图片淡出淡入
 		out(ads[startindex],400);
-		goin(ads[nextindex],400)
-		//小圆点切换
+		goin(ads[nextindex],400);
+		//小圆点样式改变
 		yuans[startindex].className="";
 		yuans[nextindex].className="liang";
+		//更改要显示图片下标
 		startindex=nextindex;
 		nextindex++;
 		if (nextindex>=len) {
 			nextindex=0;
 		}
 	};
-		
+	
 	//启动计时器实现自动轮播
 	timer=setInterval(move,3000);
 	
-	//鼠标移入移出事件
-	$(".lun").hover(function(){
-		clearInterval(timer);//结束自动轮播
-	},function(){
-		timer=setInterval(move,3000);//启动自动轮播
-	});
+	//鼠标移入移除事件
+	$(".lun").hover(function(){//移入停止轮播
+		clearInterval(timer);
+	},function(){//移除启动轮播
+		timer=setInterval(move,3000);
+	})
 	
-	//给左右绑定事件
+	//左右按钮点击事件
 	$(".left").click(function(){
 		//下一个显示的图片为当前的前面一个
 		nextindex=startindex-1;
 		if (nextindex<0) {
-			nextindex=len-1;
+			nextindex=len-1;	
 		}
 		//启动动画
 		move();
-	});
+	})
 	$(".right").click(function(){
 		move();
 	})
 	
-	//给小圆点绑定事件,添加委派
+	//小圆点移入事件，使用事件委派
 	$(".yuan").delegate("i","mouseover",function(){
-		//console.log(this);
-		for (let i=0;i<len;i++) {//遍历小圆点
-			if (this===yuans[i]) {//判断进入的小圆点是否与遍历的小圆点相同，如果相同，那么显示的小圆点下标和图片的下标设置成下一个显示
+		//遍历小圆点，判断移入的小圆点是否是当前的小圆点
+		for (var i=0;i<len;i++) {//遍历
+			//判断
+			if (this===yuans[i]) {//如果移入的小圆点是当前小圆点
+				//下一个显示的图片下标为i
 				nextindex=i;
-				move();//启动动画
-				break;//跳出遍历
+				//启动动画
+				move();
+				break;//跳出循环
 			}
 		}
 	})
-	
 }) 
