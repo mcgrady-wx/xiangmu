608207279fab73095e9496a34832196b22049a0f
xiaoming <380198622@qq.com>
2019-09-18 17:47:05 +0800

    产品页面完成

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,11 +1,17 @@
-define(["jquery", "cookie"], function() {
-	$("header").load("/html/include/header.html", function(){
-		/* 绑定搜索键盘事件 */
-		
-
-		/* 查询是否有登录用户 */
-		let user = $.cookie("loginUser");
-		if (user)
-			$(".login_reg").html(`<a href="${user}"></a>`);
+$(function(){
+	//加载头部
+	$.ajax({
+		type:"get",
+		url:"/html/include/header.html",
+		success:function(html){
+			//向body最前面加入代码
+			$("body").prepend(html);
+			// 加载 header.js 文件
+			$.getScript("/js/header.js");
+		}
 	});
-});
+	//追加尾部，另一种方法
+	$.get("/html/include/footer.html",function(html){
+		$("footer").append(html);
+	})
+})
